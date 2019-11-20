36411f957e09983426080700413c2576f9e54e92
zaqwer101 <platonov256@gmail.com>
2019-11-20 23:55:06 +0800

    Теперь с Redis

--- product_service/product/api.py
+++ product_service/product/api.py
@@ -1,23 +1,25 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import redis
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
+r = redis.Redis(host="redis-service", port=6379)
+
+
 class Product(Resource):
     def get(self):
-        return {
-            'product': [
-                'Eggs',
-                'Chocolate',
-                'Fruits',
-                'Potato',
-                'Tomato',
-                'Kek'
-            ]
-        }
+        keys = r.keys()
+        data = []
+        for key in keys:
+            if key.decode('utf-8').__contains__("product_"):
+                data.append(r.get(key).decode('utf-8'))
+        return data
+
 
 api.add_resource(Product, '/')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
